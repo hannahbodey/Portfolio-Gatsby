4291566307229622071b98f4fba2d5d6eb87bbdd
hannahbodey <hannah.bodey@live.co.uk>
2023-05-05 12:57:46 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Hannah Bodey Portfolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
